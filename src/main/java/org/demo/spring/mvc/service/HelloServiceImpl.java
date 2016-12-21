5f2c61b38cfb2abb7196bdc116dc37e40b0069ed
run-zheng <run.zheng548@gmail.com>
2016-12-22 00:11:19 +0800

    add some annotation

--- src/main/java/org/demo/spring/mvc/service/HelloServiceImpl.java
+++ src/main/java/org/demo/spring/mvc/service/HelloServiceImpl.java
@@ -1,14 +1,19 @@
 package org.demo.spring.mvc.service;
 
+import javax.annotation.Resource;
+
 import org.demo.spring.mvc.annotation.BizHandler;
 import org.springframework.stereotype.Service;
 
 @Service 
 public class HelloServiceImpl implements IHelloService {
+	@Resource
+	private IAsyncService asyncService; 
 	
 	@BizHandler
 	@Override
 	public Object sayHello(String name) {
+		asyncService.asyncProcessor(name);
 		return "Hello "+ name; 
 	}
 }
